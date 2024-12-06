eafef92cc642512e9cb32b1653e175cc947364f9
Alexis RIVIÈRE <alexis.riviere@viacesi.fr>
2024-12-06 07:02:03 +0100

    feat:eazea

--- FRONT_Nuit2Info/src/components/Globe.vue
+++ FRONT_Nuit2Info/src/components/Globe.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="$route.name !== 'infos' && $route.name !== 'resources'" ref="globeContainer" class="globe-container"></div>
+  <div v-if="$route.name !== 'infos' && $route.name !== 'resources'" ref="globeContainer" class="globe-container" id="chart"></div>
 </template>
 
 <script setup >
@@ -7,6 +7,8 @@ import { onMounted, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import Globe from 'globe.gl';
 import axios from 'axios';
+import * as THREE from '//unpkg.com/three/build/three.module.js';
+import * as satellite from 'satellite.js';
 
 const globeContainer = ref(null);
 const route = useRoute();  // Utilisation de useRoute pour accéder à la route actuelle
@@ -25,14 +27,16 @@ const getWeatherForecast = async () => {
 
 // Fonction pour créer et configurer le globe
 const createGlobe = async (comparisontype) => {
-  const globe = Globe()
-    .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
-    .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
-    // .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png')
-  (globeContainer.value);
+  if(comparisontype !=="pulmon"){
 
-  const controls = globe.controls();
-  controls.enableZoom = false;
+    const globe = Globe()
+      .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+      .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+      // .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png')
+    (globeContainer.value);
+  
+    const controls = globe.controls();
+    controls.enableZoom = false;
   if (comparisontype === "skin") {
     // Récupération des données météorologiques et affichage sur le globe
     const weatherData = await getWeatherForecast();
@@ -70,8 +74,8 @@ const createGlobe = async (comparisontype) => {
         .labelResolution(2);
     }
   }
-
-    else if(comparisontype === "heart" || comparisontype === "neurons" || comparisontype === "heart") {
+    
+    else if(comparisontype === "heart" || comparisontype === "neurons" || comparisontype === "stomac") {
       fetch('https://thingproxy.freeboard.io/fetch/https://www.submarinecablemap.com/api/v3/cable/cable-geo.json')
       .then(r => r.json())
       .then(cablesGeo => {
@@ -91,6 +95,65 @@ const createGlobe = async (comparisontype) => {
           .pathDashGap(0.008)
           .pathDashAnimateTime(12000);
 });
+      }}
+      else if(comparisontype ==="pulmon"){
+        const EARTH_RADIUS_KM = 6371; // km
+    const SAT_SIZE = 100; // km
+    const TIME_STEP = 3 * 1000; // per frame
+
+
+    const world = new Globe(document.getElementById('chart'))
+      .globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
+      .objectLat('lat')
+      .objectLng('lng')
+      .objectAltitude('alt')
+      .objectFacesSurface(false)
+      .objectLabel('name');
+
+      const controls = world.controls();
+      controls.enableZoom = false;
+
+    setTimeout(() => world.pointOfView({ altitude: 3.5 }));
+
+    const satGeometry = new THREE.OctahedronGeometry(SAT_SIZE * world.getGlobeRadius() / EARTH_RADIUS_KM / 2, 0);
+    const satMaterial = new THREE.MeshLambertMaterial({ color: 'palegreen', transparent: true, opacity: 0.7 });
+    world.objectThreeObject(() => new THREE.Mesh(satGeometry, satMaterial));
+
+    fetch('./space-track-leo.txt').then(r => r.text()).then(rawData => {
+      const tleData = rawData.replace(/\r/g, '')
+        .split(/\n(?=[^12])/)
+        .filter(d => d)
+        .map(tle => tle.split('\n'));
+      const satData = tleData.map(([name, ...tle]) => ({
+        satrec: satellite.twoline2satrec(...tle),
+        name: name.trim().replace(/^0 /, '')
+      }))
+      // exclude those that can't be propagated
+      .filter(d => !!satellite.propagate(d.satrec, new Date()).position)
+      .slice(0, 2000);
+
+      // time ticker
+      let time = new Date();
+      (function frameTicker() {
+        requestAnimationFrame(frameTicker);
+
+        time = new Date(+time + TIME_STEP);
+
+        // Update satellite positions
+        const gmst = satellite.gstime(time);
+        satData.forEach(d => {
+          const eci = satellite.propagate(d.satrec, time);
+          if (eci.position) {
+            const gdPos = satellite.eciToGeodetic(eci.position, gmst);
+            d.lat = satellite.radiansToDegrees(gdPos.latitude);
+            d.lng = satellite.radiansToDegrees(gdPos.longitude);
+            d.alt = gdPos.height / EARTH_RADIUS_KM
+          }
+        });
+
+        world.objectsData(satData);
+      })();
+    });
       }
   }
 ;
